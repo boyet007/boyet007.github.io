5c9f54440660c43a2969754751e7673d70b9ed66
Philip Loa Pleyto <boyet007@gmail.com>
2022-11-08 07:26:11 +0700

    update

--- script.js
+++ script.js
@@ -44,6 +44,8 @@ $(document).ready(function () {
         }
     });
 
+    
+
     $('.message a').click(function(){
         $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
     });
