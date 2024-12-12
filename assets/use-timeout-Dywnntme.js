8aa064bed4b4b9766a01ec6bbbb19bf560d14617
Boyet Notebook <boyet007@gmail.com>
2024-12-12 10:38:42 +0700

    update

---
+++ assets/use-timeout-Dywnntme.js
@@ -0,0 +1,1 @@
+import{e as u}from"./vm-C33txkT3.js";import{a0 as i,b as m,g as s}from"./index-C81koV2Z.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
