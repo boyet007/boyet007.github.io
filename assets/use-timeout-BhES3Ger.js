beb3126b2fae8052dcadb008b22bc03e5d06d9c2
Boyet Notebook <boyet007@gmail.com>
2024-12-12 15:17:23 +0700

    update

---
+++ assets/use-timeout-BhES3Ger.js
@@ -0,0 +1,1 @@
+import{e as u}from"./vm-CR74_sOM.js";import{a0 as i,b as m,g as s}from"./index-Bi6xh7s4.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
