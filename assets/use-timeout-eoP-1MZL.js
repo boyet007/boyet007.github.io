930711e2d1cc61578e26fcc19f200073e7e3220f
Boyet Notebook <boyet007@gmail.com>
2024-12-12 15:36:25 +0700

    update

---
+++ assets/use-timeout-eoP-1MZL.js
@@ -0,0 +1,1 @@
+import{e as u}from"./vm-X7U2GCV6.js";import{a0 as i,b as m,g as s}from"./index-B6PYZcM4.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
